3e12e57566674bda3872249a4dbaed333040f77b
Reed <112826654+ReedSkil@users.noreply.github.com>
2025-05-26 08:14:02 -0400

    Update quarry.lua
    Simplified the mine function, added the smart mine functions, and added comments

--- quarry.lua
+++ quarry.lua
@@ -49,13 +49,75 @@ function mine(width, length, newblockname)
       turtleForward.moveForward()
     end
     if i < length then
+      mineColumn(down, blockname)
       if i % 2 == 1 then
-        mineColumn(down, blockname)
         turtle.turnRight()
         turtleForward.moveForward()
         turtle.turnRight()
       else
-        mineColumn(down, blockname)
+        turtle.turnLeft()
+        turtleForward.moveForward()
+        turtle.turnLeft()
+      end
+    end
+  end
+end
+
+------------------------------------------------------------------
+--smartMine is used to dig out an area only of a specific block
+-- this is done to preserve turtle inventory space, avoid unecessary block damage, and maximize output of specific resources (sand, gravel, etc)
+
+--checks if block below turtle is the block to mine
+function isBlockBelow(blockName)
+    if turtle.detectDown() then
+        local success, blockInfo = turtle.inspectDown()
+        if success and blockInfo.name == blockName then
+            return true
+        else
+            return false
+        end
+    else
+        return false
+    end
+end
+ 
+-- Function to Mine to Depth until the specific block is found, then it backtracks to the top
+--unlike mine, this turtle backtracks to avoid mining unecessary blocks
+--a detection system can be programmed here to avoid backtracking (however this may never be done as the turtle cannot retain memory of ALL community blocks that behave like gravel/sand)
+function smartMineDepth(blockname)
+  i = 1
+  while i ~= depth do
+    if turtle.down() then
+        i = i + 1
+    else
+        if isBlockBelow(blockname) then
+            turtle.digDown()
+        else
+            break
+        end
+    end
+  end
+  i = i - 1
+  for j = 1, i do
+    turtle.up()
+  end
+end
+
+-- Function to quarry out an area of a specific bloc
+function smartMine(blockname)
+  for i = 1, length do
+    for j = 1, width do
+      smartMineDepth(blockname)
+      turtleForward.moveForward()
+    end
+    if i < length then
+      if i % 2 == 1 then
+        smartMineDepth(blockname)
+        turtle.turnRight()
+        turtleForward.moveForward()
+        turtle.turnRight()
+      else
+        smartMineDepth(blockname)
         turtle.turnLeft()
         turtleForward.moveForward()
         turtle.turnLeft()
